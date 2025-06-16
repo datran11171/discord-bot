97cb177b318ea7896f016c45445a52583f5cd214
Jamie Le Tran <jamietran200@gmail.com>
2025-06-16 21:21:38 +1000

    creating play function for music bot

--- MyBot.py
+++ MyBot.py
@@ -3,6 +3,8 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 from dotenv import load_dotenv
+import yt_dlp
+import asyncio
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -14,11 +16,47 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
+    await bot.tree.sync()
     print(f"{bot.user} is online!")
     
-@bot.event
-async def on_message(msg):
-    if msg.author.id != bot.user.id:
-        await msg.channel.send(f"Interesting message, {msg.author.mention}")
+# @bot.event
+# async def on_message(msg):
+#     if msg.author.id != bot.user.id:
+#         await msg.channel.send(f"Interesting message, {msg.author.mention}")
+
+# @bot.tree.command(name="greet", description="Sends a greeting to the user")
+# async def greet(intereaction: discord.Interaction):
+#     username = intereaction.user.mention
+#     await intereaction.response.send_message(f"Hello there, {username}")
+
+@bot.tree.command(name="play", description="Play a song or add it to the queue.")
+@app_commands.describe(song_query="Search query")
+async def play(interaction: discord.Interaction, song_query: str):
+    await interaction.response.defer()
+    
+    voice_channel = interaction.user.voice.channel
+    
+    if voice_channel is None:
+        await interaction.followup.send("You must be in a voice channel.")
+        return
+    
+    voice_client = interaction.guild.voice_client
+    
+    if voice_client is None:
+        voice_client = await voice_channel.connect()
+    elif voice_channel != voice_client.channel:
+        await voice_client.move_to(voice_channel)
+
+    ydl_options = {
+            "format": "bestaudio[abr<=96]/bestaudio",
+            "noplaylist": True,
+            "youtube_include_dash_manifest": False,
+            "youtube_include_hls_manifest": False,
+        }
+    
+    query = "ytsearch1: " + song_query
+    
+    
+
     
 bot.run(TOKEN)
